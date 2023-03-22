ff6480702226af04260977f39be09908d28181a8
Kaseke <kasymkhankhubievnis@gmail.com>
2023-03-22 18:09:20 +0700

    made a main page

--- WorkInScience/settings.py
+++ WorkInScience/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mainapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+from django.http import HttpRequest, HttpResponse
+
 # Create your views here.
+def mainpage(request: HttpRequest) -> HttpResponse:
+    return render(request, 'mainapp/mainpage.html')
