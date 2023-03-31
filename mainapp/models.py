933f2697670c2e20178b604fe5ef99f89b04851e
Kaseke <kasymkhankhubievnis@gmail.com>
2023-03-31 13:29:32 +0700

    added organization and position

--- mainapp/models.py
+++ mainapp/models.py
@@ -6,6 +6,9 @@ class User(AbstractUser):
     second_name = models.CharField(max_length=64, null=True, blank=True)
     is_activated = models.BooleanField(default=True, db_index=True, verbose_name='Прошел активацию?')
     send_messages = models.BooleanField(default=True, verbose_name='Присылать оповещения о новых событиях?')
+    position = models.CharField(max_length=64, blank=64, default='Студент',
+                                choices=(('m', 'Менеджер'),
+                                         ('s', 'Студент')))
 
     class Meta(AbstractUser.Meta):
         pass
@@ -15,19 +18,18 @@ class User(AbstractUser):
             return f'{self.first_name} {self.second_name} {self.last_name}'
         return f'{self.first_name} {self.last_name}'
 
-# class Organization(models.Model):
-#     name = models.CharField(max_length=64)
-#     bin = models.CharField(max_length=64)
-#     email_block = models.CharField(max_length=64)
-#     organization_type = models.CharField(max_length=64, blank=False,
-#                                          choices=(('None', 'Выберите должность'),
-#                                                   ('c', 'Сотрудник Компании'), 
-#                                                   ('i', 'Сотрудник Института'), 
-#                                                   ('u', 'Сотрудник Университета'), 
-#                                                   ('s', 'Студент')))
+class Organization(models.Model):
+    name = models.CharField(max_length=64)
+    bin = models.CharField(max_length=64)
+    email_block = models.CharField(max_length=64)
+    organization_type = models.CharField(max_length=64, blank=False,
+                                         choices=(('None', 'Выберите тип организации'),
+                                                  ('c', 'Компания'), 
+                                                  ('i', 'Институт'), 
+                                                  ('u', 'Университет')))
 
-#     def __str__(self) -> str:
-#         return f'Наименование: {self.name} ::: БИН {self.bin} ::: Тип {self.organization_type}'
+    def __str__(self) -> str:
+        return f'Наименование: {self.name} ::: БИН {self.bin} ::: Тип {self.organization_type}'
 
 
 class AvailableEmailDomens(models.Model):
